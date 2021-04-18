07b283fceccca1f666e01247b7c2f15a11ad95a0
Veena-S <veenasivan@gmail.com>
2021-04-18 21:48:35 +0800

    Installed currency-symbol-map and modified currency card to display the symbol

--- src/components/Currency/CurrencyCardComp.jsx
+++ src/components/Currency/CurrencyCardComp.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import './CurrencyCard.css';
 import React from 'react';
+import getSymbolFromCurrency from 'currency-symbol-map';
 
 /**
  * React component for displaying the details of a single currency
@@ -19,7 +20,11 @@ export default function CurrencyCard({ singleCurrencyData, exchangeRate }) {
         <div className="card-body currency-card-body">
           <h6 className="card-title d-flex justify-content-between">
             {singleCurrencyData.currency_code}
-            <span>{Number(exchangeRate).toFixed(Number(singleCurrencyData.decimal_units))}</span>
+            <span>
+              {Number(exchangeRate).toFixed(Number(singleCurrencyData.decimal_units))}
+              {' '}
+              {getSymbolFromCurrency(singleCurrencyData.currency_code)}
+            </span>
           </h6>
           <p className="text-muted"><small>{singleCurrencyData.currency_name}</small></p>
         </div>
